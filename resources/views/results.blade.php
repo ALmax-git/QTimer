09486cb5bd2d3604673acc8f28e46ef8b7e6e750
ALmax.git <149729893+ALmax-git@users.noreply.github.com>
2026-02-19 12:18:16 +0100

    Add Maitainance 20.2.2026

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -560,6 +560,7 @@
                                         <th style="width: 12%;">Correct</th>
                                         <th style="width: 15%;">Average (Rate)</th>
                                         <th style="width: 15%;">Average (Accuracy)</th>
+                                        <th style="width: 15%;">JAMB (400)</th>
                                         <th style="width: 8%;">Grade</th>
                                         <th style="width: 8%; text-align: center;">Details</th>
                                     </tr>
@@ -574,6 +575,7 @@
                                             <td x-text="student.summary.correct"></td>
                                             <td style="text-align: right; font-weight: 600; color: #374151;" x-text="student.summary.attempted > 0 ? (student.summary.correct / student.summary.total_questions * 100).toFixed(1) + '%' : '0.0%'"></td>
                                             <td style="text-align: right; font-weight: 600; color: #374151;" x-text="student.summary.average.toFixed(1)"></td>
+                                            <td style="text-align: right; font-weight: 600; color: #374151;" x-text="(student.summary.attempted > 0 ? (student.summary.correct / student.summary.total_questions * 100).toFixed(1))%*400"></td>
                                             <td>
                                                 <span class="grade-badge" :class="'grade-' + student.summary.grade.toLowerCase()" x-text="student.summary.grade"></span>
                                             </td>
